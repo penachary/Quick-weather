60c9faf9da1775f72d2a1a7b1074570ed8dd1c24
penachary <95870603+penachary@users.noreply.github.com>
2022-04-25 13:44:59 +0300

    added celcius and fahrenheit button
    added celcius and fahrenheit button also make switch by css and js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-"use strict"
-
-
+"use strict";
+const btnDegrees = document.querySelectorAll(".degree-symbol");
+const btnCelcius = document.getElementById("celcius");
+const btnFahrenheit = document.getElementById("fahrenheit");
+btnDegrees.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    btnCelcius.classList.toggle("switch-degree-btn");
+    btnFahrenheit.classList.toggle("switch-degree-btn");
+  })
+);
