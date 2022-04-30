69ccc83ca803744f5287d74c61b8b74b327424a0
penachary <95870603+penachary@users.noreply.github.com>
2022-04-30 09:45:09 +0300

    Content box elements rendered

--- JS/module.js
+++ JS/module.js
@@ -1,99 +1,118 @@
-import { WHEATHER_DAILY_API_URL, WHEATHER_HOURLY_API_URL, GEOCODE_API_URL, GEOCODE_API_BY_IP_URL, } from "./config.js";
+import {
+  WHEATHER_DAILY_API_URL,
+  WHEATHER_HOURLY_API_URL,
+  GEOCODE_API_URL,
+  GEOCODE_API_BY_IP_URL,
+} from "./config.js";
 
 export const stateDaily = {
-    lng:"",
-    lat:"",
-    continent: "",
-    country: "",
-    city: "",
-    localTime:"",
-    timezone:"",
-    temperature:"",
-    wheatherCode: "",
-    dayTemp: "",
-    nightTemp:"",
-    feels: "",
-    sunrise:"",
-    sunset:"",
-    pressure:"",
-    humidity:"",
-    cloudCover:"",
-    windGust:"",
-    windSpeed:"",
-    windDirection:"",
-} 
+  lng: "",
+  lat: "",
+  continent: "",
+  country: "",
+  city: "",
+  localTime: "",
+  timezone: "",
+  temperature: "",
+  wheatherCode: "",
+  dayTemp: "",
+  nightTemp: "",
+  feels: "",
+  sunrise: "",
+  sunset: "",
+  pressure: "",
+  humidity: "",
+  cloudCover: "",
+  windGust: "",
+  windSpeed: "",
+  windDirection: "",
+};
 
-const getPosition = function(){
-    return new Promise(function(resolve, reject){
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    })
-}
-     // const pos = await getPosition();
-      //   console.log(pos);
-      //   const {latitude: lat, longitude: lng} = pos.coords;
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+// const pos = await getPosition();
+//   console.log(pos);
+//   const {latitude: lat, longitude: lng} = pos.coords;
 export const geocodingAPIbyIP = async function () {
   try {
     const res = await fetch(GEOCODE_API_BY_IP_URL);
     if (!res.ok) throw new Error("problem");
     const data = await res.json();
     return data;
-
   } catch (err) {
     console.log(err);
   }
 };
 
-const getJSON = function(url, errorMsg = "Something went wrong!!!"){
-    return fetch(url).then(response => {
-        if(!response.ok) throw new Error( `${errorMsg} (${response.status})`);
-        return response.json()
-    });
+const getJSON = function (url, errorMsg = "Something went wrong!!!") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
 };
 export const wheatherForecast = async function () {
-      try{
-          const dataIP = await geocodingAPIbyIP();
-          // console.log(dataIP);
-          stateDaily.country = dataIP.country.isoName;
-          stateDaily.city = dataIP.location.city;
-          stateDaily.lng = dataIP.location.longitude;
-          stateDaily.lat = dataIP.location.latitude;
-          stateDaily.localTime = +dataIP.location.timeZone.localTime.slice(11,13);
-          stateDaily.timezone = dataIP.location.timeZone.ianaTimeId;
-          stateDaily.continent = dataIP.location.timeZone.ianaTimeId.split("/")[0];
+  try {
+    const dataIP = await geocodingAPIbyIP();
+    // console.log(dataIP);
+    stateDaily.country = dataIP.country.isoName;
+    stateDaily.city = dataIP.location.city;
+    stateDaily.lng = dataIP.location.longitude;
+    stateDaily.lat = dataIP.location.latitude;
+    stateDaily.localTime = +dataIP.location.timeZone.localTime.slice(11, 13);
+    stateDaily.timezone = dataIP.location.timeZone.ianaTimeId;
+    stateDaily.continent = dataIP.location.timeZone.ianaTimeId.split("/")[0];
 
-          const wheatherDaily = await WHEATHER_DAILY_API_URL(stateDaily.lat, stateDaily.lng, stateDaily.continent, stateDaily.city);
-          const wheatherHourly = await WHEATHER_HOURLY_API_URL(stateDaily.lat, stateDaily.lng);
-          const data = await Promise.all([getJSON(wheatherDaily), getJSON(wheatherHourly)]);
-          //  console.log(data);
-          stateDaily.dayTemp = data[0].daily.temperature_2m_max[0];
-          stateDaily.nightTemp = data[0].daily.temperature_2m_min[0];
-          stateDaily.sunrise = data[0].daily.sunrise[0].slice(11);
-          stateDaily.sunset = data[0].daily.sunset[0].slice(11);
-          stateDaily.feels = data[1].hourly.apparent_temperature[stateDaily.localTime];
-          stateDaily.cloudCover = data[1].hourly.cloudcover[stateDaily.localTime];
-          stateDaily.pressure = data[1].hourly.pressure_msl[stateDaily.localTime];
-          stateDaily.humidity = data[1].hourly.relativehumidity_2m[stateDaily.localTime];
-          stateDaily.temperature = data[1].hourly.temperature_2m[stateDaily.localTime];
-          stateDaily.wheatherCode = data[1].hourly.weathercode[stateDaily.localTime];
-          stateDaily.windDirection = data[1].hourly.winddirection_10m[stateDaily.localTime];
-          stateDaily.windGust = data[1].hourly.windgusts_10m[stateDaily.localTime];
-          stateDaily.windSpeed = data[1].hourly.windspeed_10m[stateDaily.localTime];
-          // console.log(data);
-          console.log(stateDaily);
-        }catch(err){
-            console.log(err);
-        }
-  };
-  // export const geocodingAPI = async function () {
-  //   try {
-  //     const res = await fetch(
-  //         GEOCODE_API_URL("istanbul")
-  //     );
-  //     if (!res.ok) throw new Error("problem");
-  //     const data = await res.json();
-  //     console.log(data);
+    const wheatherDaily = await WHEATHER_DAILY_API_URL(
+      stateDaily.lat,
+      stateDaily.lng,
+      stateDaily.continent,
+      stateDaily.city
+    );
+    const wheatherHourly = await WHEATHER_HOURLY_API_URL(
+      stateDaily.lat,
+      stateDaily.lng
+    );
+    const data = await Promise.all([
+      getJSON(wheatherDaily),
+      getJSON(wheatherHourly),
+    ]);
+    //  console.log(data);
+    stateDaily.dayTemp = data[0].daily.temperature_2m_max[0];
+    stateDaily.nightTemp = data[0].daily.temperature_2m_min[0];
+    stateDaily.sunrise = data[0].daily.sunrise[0].slice(11);
+    stateDaily.sunset = data[0].daily.sunset[0].slice(11);
+    stateDaily.feels =
+      data[1].hourly.apparent_temperature[stateDaily.localTime];
+    stateDaily.cloudCover = data[1].hourly.cloudcover[stateDaily.localTime];
+    stateDaily.pressure = data[1].hourly.pressure_msl[stateDaily.localTime];
+    stateDaily.humidity =
+      data[1].hourly.relativehumidity_2m[stateDaily.localTime];
+    stateDaily.temperature =
+      data[1].hourly.temperature_2m[stateDaily.localTime];
+    stateDaily.wheatherCode = data[1].hourly.weathercode[stateDaily.localTime];
+    stateDaily.windDirection =
+      data[1].hourly.winddirection_10m[stateDaily.localTime];
+    stateDaily.windGust = data[1].hourly.windgusts_10m[stateDaily.localTime];
+    stateDaily.windSpeed = data[1].hourly.windspeed_10m[stateDaily.localTime];
+    // console.log(data);
+    console.log(stateDaily);
+  } catch (err) {
+    console.log(err);
+  }
+};
+// export const geocodingAPI = async function () {
+//   try {
+//     const res = await fetch(
+//         GEOCODE_API_URL("istanbul")
+//     );
+//     if (!res.ok) throw new Error("problem");
+//     const data = await res.json();
+//     console.log(data);
 
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
--- JS/script.js
+++ JS/script.js
@@ -4,21 +4,78 @@ const btnDegrees = document.querySelectorAll(".degree-symbol");
 const btnCelcius = document.getElementById("celcius");
 const btnFahrenheit = document.getElementById("fahrenheit");
 const btnChangeBars = document.querySelectorAll(".bar");
+const firstContentContainer = document.querySelector(".first-box");
+const secondContentContainer = document.querySelector(".second-box");
 
 btnDegrees.forEach((btn) =>
   btn.addEventListener("click", function () {
     if (btn.classList.contains("switch-degree-btn")) return;
-    btnDegrees.forEach(btn => btn.classList.toggle("switch-degree-btn"))
+    btnDegrees.forEach((btn) => btn.classList.toggle("switch-degree-btn"));
   })
 );
 btnChangeBars.forEach((btn) =>
-btn.addEventListener("click", function () {
-  if (btn.classList.contains("bar-switch")) return;
-  btnChangeBars.forEach(btn => btn.classList.remove("bar-switch"))
-  btn.classList.add("bar-switch");
-})
+  btn.addEventListener("click", function () {
+    if (btn.classList.contains("bar-switch")) return;
+    btnChangeBars.forEach((btn) => btn.classList.remove("bar-switch"));
+    btn.classList.add("bar-switch");
+  })
 );
+const showWeather = async function () {
+  try {
+    await module.wheatherForecast();
+    const dailyData = module.stateDaily;
+    console.log(dailyData);
 
-// module.wheatherForecast();
+    const markup1 = `
+    <h2 class="main-adress">${dailyData.country}, ${dailyData.city}</h2>
+    <div class="content-main-box">
+      <div class="main-degree-box">
+        <span class="main-degree">${dailyData.temperature}&#176;</span>
+      </div>
+      <div class="main-icon-box">
+        <img class="main-icon" src="./icons/fog.svg" alt="Sunny">
+      </div>
+      <div class="main-wheather-detail-box"> 
+        <p class="main-wheather-detail">Sunny</p>
+      </div>
+      <div class="main-daynight-box">
+        <span class="main-daynight" >Day ${dailyData.dayTemp}&#176; - Night ${dailyData.nightTemp}&#176;</span>
+      </div>
+    </div>
+    `;
 
+    firstContentContainer.insertAdjacentHTML("afterbegin", markup1);
 
+    const markup2 = `
+    <div class="fells-sun-icons-container">
+      <div class="feels-box">
+        <p>Feels  <br> ${dailyData.feels}&#176;</p>
+      </div>
+      <div class="sunrise-set-box">
+        <div class="sunrise-box">
+          <img src="./icons/sunrise-0.svg" alt="sunrise" class="sunrise-set sunrise-icons">
+          <img src="./icons/sunset-0.svg" alt="sunset" class="sunrise-set sunrise-icons">
+        </div>
+        <div class="sunrise-box">
+          <span class="sunrise-set sunrise-time"> ${dailyData.sunrise}</span> 
+          <span class="sunrise-set sunrise-time"> ${dailyData.sunset}</span>
+        </div>
+      </div>
+    </div>
+    <ul class="list-group-daily daily-group1">
+      <li class="list-daily">Pressure ${dailyData.pressure}hPa</li>
+      <li class="list-daily">Humidity ${dailyData.humidity}%</li>
+      <li class="list-daily">Cloud cover ${dailyData.cloudCover}% </li>
+    </ul>
+    <ul class="list-group-daily daily-group2">
+      <li class="list-daily">Windgust ${dailyData.windGust}km/h</li>
+      <li class="list-daily">Windspeed  ${dailyData.windSpeed}km/h</li>
+      <li class="list-daily">Wind direction  ${dailyData.windDirection}&#176;</li>
+    </ul>
+    `;
+    secondContentContainer.insertAdjacentHTML("afterbegin", markup2);
+  } catch (err) {
+    alert(err);
+  }
+};
+showWeather();
